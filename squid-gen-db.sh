31525ad7c6f91aeb8f65d40eadb97d685db03d07
Antonie Blom <ablom@posteo.net>
2024-06-21 14:34:33 +0200

    Include global mirrors

--- squid-gen-db.sh
+++ squid-gen-db.sh
@@ -1,14 +1,52 @@
 #!/bin/bash
 set -eo pipefail
 
+#
+# Get VERSION_ID and ID
+# e.g. for Fedora 40:
+#   VERSION_ID=40
+#   ID=fedora
+#
+# The VERSION_ID doesn't matter so much, since we strip it away later
+# anyway. Its only relevance is that no all mirrors mirror all versions.
+#
+# TODO make VERSION_ID and ID overridable via cmd line
+#
 source /etc/os-release
 
+#
+# Similar to VERSION_ID, this really doesn't matter very much.
+#
 basearch=$(uname -m)
 
-( curl "https://mirrors.fedoraproject.org/metalink?repo=fedora-${VERSION_ID}&arch=${basearch}&protocol=http";
-  curl "https://mirrors.fedoraproject.org/metalink?repo=updates-released-f${VERSION_ID}&arch=${basearch}&protocol=http" ) \
+# TODO make URL overridable via cmd line
+meta_url="https://mirrors.fedoraproject.org/metalink"
+map_url="http://${ID}.mirrors.squid.internal"
+
+# We presume Squid will only be used to cache plain HTTP downloads, not
+# HTTPS/other methods. Filter mirrors accordingly.
+meta_url="${meta_url}?protocol=http"
+
+# The metalink will return the best mirrors for your given location, and
+# exclude ones that are far away. Since that list isn't necessarily very
+# stable, just include all global mirrors.
+meta_url="${meta_url}&country=global"
+
+# The arch is a required parameter.
+meta_url="${meta_url}&arch=${basearch}"
+
+#
+# Fetch the metalinks for normal repo and updates, find lines containing
+# suitable URLs, trim them so that only the root remains, escape them
+# for use in storeid_file_rewrite regexes, and tack on everything else
+# required by storeid_file_rewrite. Sort and remove duplicates.
+#
+# In July 2024 on Fedora 40 x86_64, the final output has 165 entries.
+#
+( curl -sS "${meta_url}&repo=fedora-${VERSION_ID}";
+  curl -sS "${meta_url}&repo=updates-released-f${VERSION_ID}" ) \
 	| grep 'http://.*\(releases\|updates\)/[0-9]\+/Everything' \
 	| sed 's@.*>\([^<]*/\)\(releases\|updates\)/[0-9]\+/Everything.*$@\1@' \
 	| sed 's/[]\/$*.^[]/\\&/g' \
-	| awk '{ print "^" $0 "(.*\\.rpm)$\thttp://fedora.mirrors.squid.internal/$1"; }' \
+	| awk "{ print \"^\" \$0 \"(.*\\\\.rpm)$\\t${map_url}/\$1\"; }" \
 	| sort -u
